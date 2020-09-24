782cd030ca270eabeaa902677d295ed035b213eb
Pedro Vanzo <pedro_vanzo@hotmail.com>
2020-09-24 02:46:09 -0300

    Tomando forma, estilizando a lista

--- script.js
+++ script.js
@@ -1,47 +1,27 @@
-// Função para criar os elementos no DOM
-function createElement(tag, className) {
-  const element = document.createElement(tag);
-  if (className) element.classList.add(className);
-
-  return element;
-}
-
-function loadPokedex() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      //   document.getElementById("demo").innerHTML = this.responseText;
-      console.log(this.responseText);
-    }
-  };
-  xhttp.open(
-    "GET",
-    "https://pokeapi.co/api/v2/pokemon?limit=100&offset=200",
-    true
-  );
-  xhttp.send();
-}
-
+// Objeto Pokedex
 let pokedexObj = new Object();
+
+// Requisição Ajax
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
+  // Variavel para manipular o DOM
+  var x = document.getElementById("root");
+
+  // Sucesso na requisição
   if (this.readyState == 4 && this.status == 200) {
-    //   console.log(this.responseText)
-      pokedexObj = JSON.parse(this.responseText);
-      console.log(pokedexObj.results)
-    // pokedexObj = this.responseText;
-    //   console.log(pokedexObj);
-    // console.log(pokedexObj);
-    // console.log(pokedexObj);
-    // for(var i = 0; i < 10; i++){
-    // console.log(pokedexObj);
-    // }
+    // Objeto recebe a resposta da requisição
+    pokedexObj = JSON.parse(this.responseText);
+
+    // Criando um elemento para cada item do objeto pokedexObj
+    for (var i = 0; i < 2; i++) {
+      console.log(pokedexObj.results[i]);
+      var y = document.createElement("li");
+      y.innerText = pokedexObj.results[i].name;
+      x.appendChild(y);
+    }
 
     // for each element, create an li with pokedexObj.results[i];
-    document.getElementById("root").innerHTML = JSON.stringify(pokedexObj.results);
-    // document.getElementById("root").innerHTML = JSON.stringify(pokedexObj.results[6]);
-    // console.log(this.responseText);
-    // console.log(pokedexObj);
+    // document.getElementById("root").innerHTML = JSON.stringify(pokedexObj.results);
   }
 };
 xhttp.open(
