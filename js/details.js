d12897e964fbd00eb5b5d1bc44fffd36ada9e679
Pedro Vanzo <pedro_vanzo@hotmail.com>
2020-09-27 19:56:30 -0300

    Index completo, ajustes no css

--- js/details.js
+++ js/details.js
@@ -6,10 +6,10 @@ let pokemon = document.location.search.replace(/^.*?\=/, "");
 let pokedexObj = new Object();
 
 // Requisição Ajax Pokedex
-var xhttp = new XMLHttpRequest();
+let xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   // Variavel para manipular o DOM
-  var ul = document.getElementById("root");
+  let ul = document.getElementById("root");
 
   // Sucesso na requisição
   if (this.readyState == 4 && this.status == 200) {
@@ -23,84 +23,92 @@ xhttp.onreadystatechange = function() {
     // document.getElementById("root").appendChild(pokeLi);
 
     //name
-    document.getElementById("name").innerHTML = pokedexObj.name.charAt(0).toUpperCase() + pokedexObj.name.slice(1);
+    document.getElementById("name").innerHTML =
+      pokedexObj.name.charAt(0).toUpperCase() + pokedexObj.name.slice(1);
 
     //page title
-    document.title = pokedexObj.name.charAt(0).toUpperCase() + pokedexObj.name.slice(1);
+    document.title =
+      pokedexObj.name.charAt(0).toUpperCase() + pokedexObj.name.slice(1);
     //img
-    document.getElementById("pokeImg").setAttribute("src", "https://pokeres.bastionbot.org/images/pokemon/" + pokemon + ".png");
-    
-    
-    
+    document
+      .getElementById("pokeImg")
+      .setAttribute(
+        "src",
+        "https://pokeres.bastionbot.org/images/pokemon/" + pokemon + ".png"
+      );
+
     // habilidades
-    console.log(pokedexObj.abilities)
+    console.log(pokedexObj.abilities);
     abilityCount = Object.keys(pokedexObj.abilities);
     console.log(abilityCount.length);
-    if(abilityCount.length>1){
-        if(abilityCount.length<4){
-            for(let i = 0; i < abilityCount.length; i++){
-                let abDiv = document.createElement("div");
-                abDiv.innerHTML = pokedexObj.abilities[i].ability.name;
-                document.getElementById("ability").appendChild(abDiv);
-            }
-        }else{
-            for(let i = 0; i < 4; i++){
-                // console.log(pokedexObj.moves[i].move);
-                // console.log(pokedexObj.moves[i].move.name);
-                let abDiv = document.createElement("div");
-                // moveDiv.innerHTML = pokedexObj.moves[i].move.name;
-                // insere 4 ataques aleatorios da lista de ataques
-                abDiv.innerHTML = pokedexObj.abilities[Math.floor(Math.random() * (abilityCount.length - 1)) + 1].ability.name;
-                document.getElementById("ability").appendChild(abDiv);
-            }
+    if (abilityCount.length > 1) {
+      if (abilityCount.length < 4) {
+        for (let i = 0; i < abilityCount.length; i++) {
+          let abDiv = document.createElement("div");
+          abDiv.innerHTML = pokedexObj.abilities[i].ability.name;
+          document.getElementById("ability").appendChild(abDiv);
         }
-    }else{
-        let abDiv = document.createElement("div");
-        // console.log('igual ou menor que 1');
-        console.log(pokedexObj.abilities[0].ability.name);
-        
-        abDiv.innerHTML = pokedexObj.abilities[0].ability.name;
-        document.getElementById("ability").appendChild(abDiv);
+      } else {
+        for (let i = 0; i < 4; i++) {
+          // console.log(pokedexObj.moves[i].move);
+          // console.log(pokedexObj.moves[i].move.name);
+          let abDiv = document.createElement("div");
+          // moveDiv.innerHTML = pokedexObj.moves[i].move.name;
+          // insere 4 ataques aleatorios da lista de ataques
+          abDiv.innerHTML =
+            pokedexObj.abilities[
+              Math.floor(Math.random() * (abilityCount.length - 1)) + 1
+            ].ability.name;
+          document.getElementById("ability").appendChild(abDiv);
+        }
+      }
+    } else {
+      let abDiv = document.createElement("div");
+      // console.log('igual ou menor que 1');
+      console.log(pokedexObj.abilities[0].ability.name);
+
+      abDiv.innerHTML = pokedexObj.abilities[0].ability.name;
+      document.getElementById("ability").appendChild(abDiv);
     }
-    
-    
+
     //moves => for loop to go through moves list, adding 4 moves (maybe random)
     // console.log(pokedexObj.moves)
-    var movesCount = Object.keys(pokedexObj.moves);
+    let movesCount = Object.keys(pokedexObj.moves);
     console.log(movesCount.length);
     // corrige se o pokemon tiver só um ataque
-    if(movesCount.length>1){
-        console.log('maior que 1');
-        for(let i = 0; i < 4; i++){
-            // console.log(pokedexObj.moves[i].move);
-            // console.log(pokedexObj.moves[i].move.name);
-            let moveDiv = document.createElement("div");
-            // moveDiv.innerHTML = pokedexObj.moves[i].move.name;
-            // insere 4 ataques aleatorios da lista de ataques
-            moveDiv.innerHTML = pokedexObj.moves[Math.floor(Math.random() * (movesCount.length - 1)) + 1].move.name;
-            document.getElementById("move").appendChild(moveDiv);
-        }
-    }else{
+    if (movesCount.length > 1) {
+      console.log("maior que 1");
+      for (let i = 0; i < 4; i++) {
+        // console.log(pokedexObj.moves[i].move);
+        // console.log(pokedexObj.moves[i].move.name);
         let moveDiv = document.createElement("div");
-        // console.log('igual ou menor que 1');
-        moveDiv.innerHTML = pokedexObj.moves[0].move.name;
+        // moveDiv.innerHTML = pokedexObj.moves[i].move.name;
+        // insere 4 ataques aleatorios da lista de ataques
+        moveDiv.innerHTML =
+          pokedexObj.moves[
+            Math.floor(Math.random() * (movesCount.length - 1)) + 1
+          ].move.name;
         document.getElementById("move").appendChild(moveDiv);
+      }
+    } else {
+      let moveDiv = document.createElement("div");
+      // console.log('igual ou menor que 1');
+      moveDiv.innerHTML = pokedexObj.moves[0].move.name;
+      document.getElementById("move").appendChild(moveDiv);
     }
-    
-
   }
 };
 xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon/" + pokemon, true);
 xhttp.send();
 
 let pokemonNumber = parseInt(pokemon);
-function previousP(){
-    // console.log(pokemonNumber);
-    // console.log(pokemonNumber - 1);
-    window.location.replace("./details.html?id=" + (pokemonNumber - 1));
+function previousP() {
+  // console.log(pokemonNumber);
+  // console.log(pokemonNumber - 1);
+  window.location.replace("./details.html?id=" + (pokemonNumber - 1));
 }
-function nextP(){
-    // console.log(pokemonNumber);
-    // console.log(pokemonNumber + 1);
-    window.location.replace("./details.html?id=" + (pokemonNumber + 1));
+function nextP() {
+  // console.log(pokemonNumber);
+  // console.log(pokemonNumber + 1);
+  window.location.replace("./details.html?id=" + (pokemonNumber + 1));
 }
--- js/pokedex.js
+++ js/pokedex.js
@@ -1,94 +1,123 @@
 // Objeto Pokedex
 let pokedexObj = new Object();
-
-// Requisição Ajax Pokedex
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
+// Xhttp
+let xhttp = new XMLHttpRequest();
+// Função para carregar Pokedex
+function loadPokedex(gen) {
   // Variavel para manipular o DOM
-  var ul = document.getElementById("root");
-
-  // Sucesso na requisição
-  if (this.readyState == 4 && this.status == 200) {
-    // Objeto recebe a resposta da requisição
-    pokedexObj = JSON.parse(this.responseText);
-    // console.log(pokedexObj);
-
-    // variavel para controlar quantos pokemons exibir
-    var pokeCount = Object.keys(pokedexObj.results);
-    // console.log(pokeCount);
-
-    // Criando um elemento para cada item do objeto pokedexObj
-    for (var i = 0; i < pokeCount.length; i++) {
-
-      // Pega o id de cada pokemon
-      let pokeId = pokedexObj.results[i].url.split('/');
-      // console.log(pokeId[6]);
-
-
-      // Cria li
-      // console.log(pokedexObj.results[i]);
-      var li = document.createElement("li");
-      li.classList.add("later");
-      // li.innerText = pokedexObj.results[i].name;
-      // Insere no DOM
-      ul.appendChild(li);
-
-      // Cria div img
-      var divImg = document.createElement("div");
-      divImg.classList.add("laterImg");
-      // Cria img dentro da img
-      var imgSrc = document.createElement("img");
-      // Insere a url na img
-      imgSrc.setAttribute(
-        "src",
-        "https://pokeres.bastionbot.org/images/pokemon/" + (pokeId[6]) + ".png"
-      );
-      imgSrc.classList.add("pokedex-arrow-img");
-      // Insere no DOM
-      li.appendChild(divImg);
-      divImg.appendChild(imgSrc);
-
-      // Cria div Nome
-      var divTitle = document.createElement("div");
-      divTitle.classList.add("pokedex-title");
-      // Cria o conteudo da div
-      titleContent = document.createTextNode(pokedexObj.results[i].name.charAt(0).toUpperCase() + pokedexObj.results[i].name.slice(1));
-      // Insere no DOM
-      divTitle.appendChild(titleContent);
-      li.appendChild(divTitle);
-
-      // Cria div Seta
-      var divSeta = document.createElement("div");
-      // Cria anchor
-      var anchor = document.createElement("a");
-      anchor.href = "./details.html";
-      
-      // console.log(pokeId[6]);
-      
-      anchor.href = "./details.html" + "?id=" + pokeId[6];
-
-      // anchor.href = "https://pokeapi.co/api/v2/pokemon/" + pokeId[6];
-
-      // anchor.href = pokedexObj.results[i].url;
-      // Cria img seta
-      var anchorImg = document.createElement("img");
-      anchorImg.setAttribute(
-        "src",
-        "./assets/img/arrow.png"
-      );
-      anchorImg.classList.add("pokedex-arrow-img");
-      // Insere no DOM
-      anchor.appendChild(anchorImg);
-      divSeta.appendChild(anchor);
-      li.appendChild(divSeta);
-    }
+  let ul = document.getElementById("root");
+  // Limpando a pokedex para mudar de geração
+  let listLength = ul.children.length;
+  for (i = 0; i < listLength; i++) {
+    ul.removeChild(ul.children[0]);
   }
-};
-xhttp.open(
-  "GET",
-  "https://pokeapi.co/api/v2/pokemon?limit=100&offset=200",
-  // Make a different folder with 1gen pokemon VVV
-  // "https://pokeapi.co/api/v2/pokemon?limit=151",
-  true
-);
-xhttp.send();
+  // Requisição
+  xhttp.onreadystatechange = function() {
+    // Sucesso na requisição
+    if (this.readyState == 4 && this.status == 200) {
+      // Objeto recebe a resposta da requisição
+      pokedexObj = JSON.parse(this.responseText);
+      // Variavel para controlar quantos pokemons exibir
+      let pokeCount = Object.keys(pokedexObj.results);
+      // Criando um elemento para cada item do objeto pokedexObj
+      for (let i = 0; i < pokeCount.length; i++) {
+        // Pega o id de cada pokemon
+        let pokeId = pokedexObj.results[i].url.split("/");
+        // pokeId[6] = pokeId;
+        // Cria li do pokemon
+        let li = document.createElement("li");
+        li.classList.add("later");
+        // Insere no DOM
+        ul.appendChild(li);
+        // Cria div da img
+        let divImg = document.createElement("div");
+        divImg.classList.add("laterImg");
+        // Cria img
+        let imgSrc = document.createElement("img");
+        imgSrc.setAttribute(
+          "src",
+          "https://pokeres.bastionbot.org/images/pokemon/" + pokeId[6] + ".png"
+        );
+        imgSrc.classList.add("pokedex-arrow-img");
+        // Insere no DOM
+        li.appendChild(divImg);
+        divImg.appendChild(imgSrc);
+        // Cria div Nome
+        let divTitle = document.createElement("div");
+        divTitle.classList.add("pokedex-title");
+        // Cria o conteudo da div
+        titleContent = document.createTextNode(
+          pokedexObj.results[i].name.charAt(0).toUpperCase() +
+            pokedexObj.results[i].name.slice(1)
+        );
+        // Insere no DOM
+        divTitle.appendChild(titleContent);
+        li.appendChild(divTitle);
+        // Cria div Seta
+        let divSeta = document.createElement("div");
+        // Cria anchor
+        let anchor = document.createElement("a");
+        // Adiciona pokeId na URL
+        anchor.href = "./details.html" + "?id=" + pokeId[6];
+        // Cria img seta
+        let anchorImg = document.createElement("img");
+        anchorImg.setAttribute("src", "./assets/img/arrow.png");
+        anchorImg.classList.add("pokedex-arrow-img");
+        // Insere no DOM
+        anchor.appendChild(anchorImg);
+        divSeta.appendChild(anchor);
+        li.appendChild(divSeta);
+      }
+    }
+  };
+  // Gerações diferentes
+  let offsetPokemon = 0;
+  let limitPokemon = 0;
+  if (gen == 1) {
+    // Valores para geração 1
+    limitPokemon = 151;
+    offsetPokemon = 0;
+  } else if (gen == 2) {
+    // Valores para geração 2
+    limitPokemon = 100;
+    offsetPokemon = 151;
+  } else if (gen == 3) {
+    // Valores para geração 3
+    limitPokemon = 135;
+    offsetPokemon = 251;
+  } else {
+    // Valores padrão
+    limitPokemon = 100;
+    offsetPokemon = 200;
+  }
+  // Ajax
+  xhttp.open(
+    "GET",
+    "https://pokeapi.co/api/v2/pokemon?limit=" +
+      limitPokemon +
+      "&offset=" +
+      offsetPokemon,
+    true
+  );
+  xhttp.send();
+}
+// Escolher gerações
+let coll = document.getElementsByClassName("collapsible");
+let i;
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = document.getElementById("content");
+    if (content.style.display === "block") {
+      content.style.display = "none";
+      document.getElementById("symbol").innerHTML = "&#9660;";
+    } else {
+      content.style.display = "block";
+      document.getElementById("symbol").innerHTML = "&#9932;";
+    }
+  });
+}
+// Função fake para Sign Out
+function fakeSignOut() {
+  window.location.replace("./index.html");
+}
